9a9fe093d3780f4d614cc76eea237cb5c9d5f199
Jay8303 <107171637+Jay8303@users.noreply.github.com>
2025-03-11 20:34:28 +0530

    Update before_install.sh

--- src/components/scripts/before_install.sh
+++ src/components/scripts/before_install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Cleaning old application files..."
+echo "Running before_install.sh..."
+
+# Take ownership of the directory (recursive)
+sudo chown -R ec2-user:ec2-user /home/ec2-user/my-app
+
+# Now it's safe to remove contents
 rm -rf /home/ec2-user/my-app/*
+
+echo "Cleaned up old files in /home/ec2-user/my-app"
