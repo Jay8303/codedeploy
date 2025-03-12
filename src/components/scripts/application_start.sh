6251680d2f7807059ad28f489b0b641b8506d58b
Jay8303 <107171637+Jay8303@users.noreply.github.com>
2025-03-12 10:39:49 +0530

    Update application_start.sh

--- src/components/scripts/application_start.sh
+++ src/components/scripts/application_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/my-app
-pm2 start app.js
+cd /home/ec2-user/my-app  # Make sure this is the correct path where files are extracted
+npm install
+npm run build
+serve -s build -l 3000  # This will serve on port 3000
