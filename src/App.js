a9eb40fb5ee57024adf10ea25f37b54d2bb850b8
Jay8303 <107171637+Jay8303@users.noreply.github.com>
2025-03-12 11:37:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import LoginForm from './components/LoginForm/LoginForm';
 import RegisterForm from './components/LoginForm/Register';
 import ForgotPassword from './components/LoginForm/ForgotPassword';
 import Home from './components/LoginForm/Home';
