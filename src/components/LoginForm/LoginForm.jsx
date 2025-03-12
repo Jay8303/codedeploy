5718d9ad173d9dd9e177645d29262d9df651dcd0
Jay8303 <107171637+Jay8303@users.noreply.github.com>
2025-03-12 10:47:24 +0530

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Loginform.css';
+import './LoginForm.css';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
