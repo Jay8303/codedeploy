da15d532f101da408e863c1dc550c7ce18823c61
Jay8303 <107171637+Jay8303@users.noreply.github.com>
2025-03-12 09:58:51 +0530

    Update application_start.sh

--- src/components/scripts/application_start.sh
+++ src/components/scripts/application_start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "Starting application with PM2..."
-pm2 stop all
-pm2 start /home/ec2-user/my-app/app.js  # Ensure this file exists
-npm install
-npm start
+cd /home/ec2-user/my-app
+pm2 start app.js
